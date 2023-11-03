4d26442eef7a3096386925557cce9c844fef52cc
CAHC2509 <111461283+CAHC2509@users.noreply.github.com>
2023-11-03 17:56:33 -0500

    First-Person Player Controller Implementation
    Added a first-person player controller script, allowing for smooth character movement, jumping, and camera control in a 3D environment.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement and jump settings")]
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private float walkSpeed = 6f;
+    [SerializeField] private float runSpeed = 12f;
+    [SerializeField] private float jumpPower = 7f;
+    [SerializeField] private float gravity = 10f;
+
+    [Space, Header("Camera settings")]
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float lookSpeed = 2f;
+    [SerializeField] private float lookXLimit = 45f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+
+        // Handles player movment
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical");
+        float curSpeedY = (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal");
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        // Handles player jumping
+        if (Input.GetButton("Jump") && characterController.isGrounded)
+            moveDirection.y = jumpPower;
+        else
+            moveDirection.y = movementDirectionY;
+
+        if (!characterController.isGrounded)
+            moveDirection.y -= gravity * Time.deltaTime;
+
+        // Apply movement
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        // Handles camera rotation
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+    }
+}
